781a4643974e2c9a777592d859879517b4e4f78b
Blerand Gashi <blerandgashi15@gmail.com>
2025-12-14 04:08:04 +0100

    added async js

--- script.js
+++ script.js
@@ -4,27 +4,35 @@ let temperatureEl = document.querySelector("#temp");
 let weatherDescription = document.querySelector("#description");
 let humidity = document.querySelector("#humidity");
 let windSpeed = document.querySelector("#wind-speed");
-
+let error = document.querySelector("#error");
+let weatherInfo = document.querySelector("#weather-info");
 
 const apiKey = '238bc99d0a21b2eab9461db3149a186b';
 const searchBtn = document.querySelector("#search-btn");
 
+async function getWeatherInfos() {
+  try{
+
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInputEl.value}&appid=${apiKey}&units=metric`)
+    const data = await response.json();
+
+    console.log(data);
+    cityName.textContent = data.name;  
+    temperatureEl.textContent = data.main.temp + "°C";
+    weatherDescription.textContent = data.weather[0].description;
+    humidity.textContent = data.main.humidity + "%";
+    windSpeed.textContent = data.wind.speed + " km/h";
+
+    weatherInfo.classList.add("active");
+    error.classList.remove("active");
+
+  }catch (error){
+    weatherInfo.classList.remove("active");
+    error.classList.add("active");
+  }
+}
+
 searchBtn.addEventListener("click", function(){
-  let cityInputElValue = cityInputEl.value;
-
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityInputElValue}&appid=${apiKey}&units=metric`)
-    .then(response => response.json())
-    .then(data => {
-
-      console.log(data);
-      cityName.textContent = data.name;  
-      temperatureEl.textContent = data.main.temp + "°C";
-      weatherDescription.textContent = data.weather[0].description;
-      humidity.textContent = data.main.humidity + "%";
-      windSpeed.textContent = data.wind.speed + " km/h";
-
-    })
-
-    let weatherInfo = document.querySelector("#weather-info");
-    weatherInfo.classList.add("active")
+  getWeatherInfos();
+  cityInputEl.value = ""
 })
